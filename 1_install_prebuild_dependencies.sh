3146c44bf2410b77f0bf96fb13cdc77f86bed5b9
foxaice <foxaice@yandex.com>
2023-03-28 20:26:01 +0700

    updated prebuild dependecies to build WebRTC

--- 1_install_prebuild_dependencies.sh
+++ 1_install_prebuild_dependencies.sh
@@ -4,11 +4,16 @@ echo "The following dependencies are required to start building:"
 echo "* python2.7"
 echo "* openjdk-8-jdk"
 echo "* git"
+echo "* ninja-build"
+echo "* libgd3"
+echo "* libgl1-mesa-dri"
+echo "* libglapi-mesa"
+echo "* libglx-mesa0"
 echo ""
 echo "To install them You might have to enter your password for 'sudo'."
-#TODO to find which packages are needed (sudo apt install -y libgd3 libgl1-mesa-dri libglapi-mesa libglx-mesa0)??
 sudo apt-get -y update
-sudo apt-get -y install python2.7 openjdk-8-jdk git
+sudo apt-get -y install python2.7 openjdk-8-jdk git ninja-build
+sudo apt-get -y install libgd3 libgl1-mesa-dri libglapi-mesa libglx-mesa0
 
 if [[ -d ./depot_tools ]]
 then
