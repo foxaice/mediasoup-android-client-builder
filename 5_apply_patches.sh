7962e0902e423184b3a7a5a635f42aa64e98632d
Dmitry <dmitry.moiseenko@sequenia.com>
2024-02-08 10:30:06 +0700

    changed path for support_default_audio_device_module_for_webrtc_voice_engine.patch

--- 5_apply_patches.sh
+++ 5_apply_patches.sh
@@ -5,7 +5,7 @@
 # "git apply abc.patch || git apply abc.patch -R --check && echo already applied"
 # But this behavior allows you to see real errors during the first patch.
 
-
+echo "Apply webrtc h264 support patch"
 cd $SCRIPT_DIR/webrtc_android/src/third_party/ffmpeg
 git apply $SCRIPT_DIR/webrtc_h264_support.patch
 
@@ -13,6 +13,7 @@ git apply $SCRIPT_DIR/webrtc_h264_support.patch
 # Who use "use_custom_libcxx=false" argument build for android
 # got "undefined symbol: _Unwind_Backtrace" mistake should do like this:
 # https://bugs.chromium.org/p/webrtc/issues/detail?id=13535
+echo "Apply use custom libcxx argument support patch"
 cd $SCRIPT_DIR/webrtc_android/src/buildtools
 git apply $SCRIPT_DIR/use_custom_libcxx_argument_buildtools_support.patch
 
@@ -23,6 +24,6 @@ git apply $SCRIPT_DIR/use_custom_libcxx_argument_build_support.patch
 #webrtc_voice_engine.cc create a default AudioDeviceModule if no supplied.
 #    adm_ = webrtc::AudioDeviceModule::Create(
 #       webrtc::AudioDeviceModule::kPlatformDefaultAudio, task_queue_factory_)
-cd $SCRIPT_DIR/webrtc_android/src/media/engine
+echo "Apply support default audio device module patch"
+cd $SCRIPT_DIR/webrtc_android/src
 git apply $SCRIPT_DIR/support_default_audio_device_module_for_webrtc_voice_engine.patch
-
